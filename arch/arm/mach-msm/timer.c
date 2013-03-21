3792b6d390932e646bbd3437ee2ba982c17aeb03
xcstacy <zkchong2005@hotmail.com>
2013-03-21 00:21:15 +0000

    Merge msm: timer: Use 32768Hz GPT and SCLK on 8064 family for Mako

--- arch/arm/mach-msm/timer.c
+++ arch/arm/mach-msm/timer.c
@@ -1057,9 +1057,11 @@ static void __init msm_timer_init(void)
 		__raw_writel(DGT_CLK_CTL_DIV_4, MSM_TMR_BASE + DGT_CLK_CTL);
 		gpt->status_mask = BIT(10);
 		dgt->status_mask = BIT(2);
+	if (!cpu_is_apq8064()) {
 		gpt->freq = 32765;
 		gpt_hz = 32765;
 		sclk_hz = 32765;
+	}
 		gpt->flags |= MSM_CLOCK_FLAGS_UNSTABLE_COUNT;
 		dgt->flags |= MSM_CLOCK_FLAGS_UNSTABLE_COUNT;
 	} else if (cpu_is_msm8960() || cpu_is_apq8064() || cpu_is_msm8930() ||
